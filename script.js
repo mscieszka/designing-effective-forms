115390c0a9b8baa4aadff20c15b3d2904b709ad2
mscieszka <scieszka.marcin@pm.me>
2025-05-23 23:31:35 +0200

    improve country select form using choices js library, fill country code based on country

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let clickCount = 0;
+let choicesInstance;
 
 const countryInput = document.getElementById('country');
 const myForm = document.getElementById('form');
@@ -18,8 +19,25 @@ async function fetchAndFillCountries() {
             throw new Error('Błąd pobierania danych');
         }
         const data = await response.json();
-        const countries = data.map(country => country.name.common);
-        countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
+        const countries = data.map(country => country.name.common).sort();
+        countryInput.innerHTML = '<option value="">Wybierz kraj</option>' +
+            countries.map(country => `<option value="${country}">${country}</option>`).join('');
+
+        // Inicjalizacja Choices.js
+        if (choicesInstance) {
+            choicesInstance.destroy();
+        }
+        choicesInstance = new Choices(countryInput, {
+            searchEnabled: true,
+            itemSelectText: '',
+            shouldSort: false,
+        });
+        countryInput.addEventListener('change', (e) => {
+            const selectedCountry = e.target.value;
+            if (selectedCountry) {
+                getCountryCode(selectedCountry);
+            }
+        });
     } catch (error) {
         console.error('Wystąpił błąd:', error);
     }
@@ -29,34 +47,51 @@ function getCountryByIP() {
     fetch('https://get.geojs.io/v1/ip/geo.json')
         .then(response => response.json())
         .then(data => {
-            const country = data.country
-            countryInput.value = country;
-            getCountryCode(country)
+            const country = data.country;
+
+            if (choicesInstance) {
+                const option = Array.from(countryInput.options).find(opt => opt.value === country);
+                if (option) {
+                    choicesInstance.setChoiceByValue(country);
+                    getCountryCode(country);
+                }
+            } else {
+                countryInput.value = country;
+                getCountryCode(country);
+            }
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
         });
 }
-
 function getCountryCode(countryName) {
     const apiUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
 
     fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Błąd pobierania danych');
-        }
-        return response.json();
-    })
-    .then(data => {        
-        const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        countryCodeInput.value = countryCode
-    })
-    .catch(error => {
-        console.error('Wystąpił błąd:', error);
-    });
-}
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Błąd pobierania danych');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const code = data[0].idd.root; // Only the root
+            const optionExists = Array.from(countryCodeInput.options).some(opt => opt.value === code);
 
+            if (optionExists) {
+                countryCodeInput.value = code;
+            } else {
+                const newOption = document.createElement("option");
+                newOption.value = code;
+                newOption.textContent = `${code} (${countryName})`;
+                countryCodeInput.appendChild(newOption);
+                countryCodeInput.value = code;
+            }
+        })
+        .catch(error => {
+            console.error('Wystąpił błąd:', error);
+        });
+}
 
 (() => {
     // nasłuchiwania na zdarzenie kliknięcia myszką
